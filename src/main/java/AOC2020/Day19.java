e7c352d927a0094df07a1d59ac8fe9b6d4f27270
Ozin <Ozin@users.noreply.github.com>
2020-12-30 23:04:39 +0100

    Draft Day19B

--- src/main/java/AOC2020/Day19.java
+++ src/main/java/AOC2020/Day19.java
@@ -1,16 +1,15 @@
 package AOC2020;
 
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import one.util.streamex.EntryStream;
 import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
 import utils.AbstractDay;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day19 extends AbstractDay<Day19.Input> {
     public static void main(final String[] args) {
@@ -24,63 +23,143 @@ public class Day19 extends AbstractDay<Day19.Input> {
             .map(s -> s.split(": "))
             .toMap(s -> Integer.parseInt(s[0]), s -> s[1]);
 
-
-        return new Input(rawRuleMap, Arrays.asList(blocks[1].split("\n")));
+        return new Input(Rule.of(rawRuleMap), Arrays.asList(blocks[1].split("\n")));
     }
 
     @Override
     protected Object a(final Input input) throws Exception {
-        final Map<Integer, String> rules = input.getRules();
-        final String rule = getRule(0, rules, new HashMap<Integer, String>());
-        final Predicate<String> pattern = Pattern.compile("^" + rule + "$").asPredicate();
+        final Map<Integer, Rule> rules = input.getRules();
 
-        return StreamEx.of(input.getMessages()).filter(pattern).count();
-    }
-
-    private String getRule(final int i, final Map<Integer, String> rawRules, final HashMap<Integer, String> cache) {
-        String rule = cache.get(i);
-        if (rule != null) {
-            return rule;
-        }
-
-        final String rawRule = rawRules.get(i);
-
-        if (rawRule.matches("\".\"")) {
-            final String ab = Character.toString(rawRule.charAt(1));
-            cache.put(i, ab);
-            return ab;
-        }
-
-        final String[] ors = rawRule.split(" ?\\| ?");
-
-        rule = StreamEx.of(ors)
-            .map(subRuleGroup -> StreamEx.of(subRuleGroup.split(" ")).mapToInt(Integer::parseInt).toArray())
-            .map(subRules -> assembleRule(rawRules, cache, subRules))
-            .joining("|");
-
-        cache.put(i, rule);
-        return rule;
-    }
-
-    private String assembleRule(final Map<Integer, String> rawRules, final HashMap<Integer, String> cache, final int[] subRules) {
-        return IntStreamEx.of(subRules)
-            .mapToObj(next -> getRule(next, rawRules, cache))
-            .map(next -> "(" + next + ")")
-            .joining();
+        return StreamEx.of(input.getMessages())
+            .filter(message -> input.getRules().get(0).matches(message, input.getRules()))
+            .count();
     }
 
     @Override
     protected Object b(final Input input) throws Exception {
-        // aa | aa+
-        // aacc || aaaacccc
-        input.getRules().put(8, "42 | 42 8");
-        input.getRules().put(11, "42 31 | 42 11 31");
-        return null;
+
+        // 8, "42 | 42 8"
+        input.getRules().put(8, new OrRule(new AndRule(42), new AndRule(42, 8)));
+        // 11, "42 31 | 42 11 31"
+        input.getRules().put(11, new OrRule(new AndRule(42, 31), new AndRule(42, 11, 31)));
+
+        return StreamEx.of(input.getMessages())
+            .filter(message -> input.getRules().get(0).matches(message, input.getRules()))
+            .count();
     }
 
     @Value
     public static class Input {
-        Map<Integer, String> rules;
+        Map<Integer, Rule> rules;
         List<String> messages;
+    }
+
+    interface Rule {
+        static Map<Integer, Rule> of(final Map<Integer, String> rawRuleMap) {
+            return EntryStream.of(rawRuleMap)
+                .mapValues(Rule::getStringOrRuleFunction)
+                .toMap();
+        }
+
+        private static Rule getStringOrRuleFunction(final String rawRule) {
+            if (rawRule.matches("\".\"")) {
+                return new ConstRule(rawRule.substring(1, 2));
+            }
+
+            final String[] orRules = rawRule.split(" \\| ");
+
+            if (orRules.length > 1) {
+                return new OrRule(StreamEx.of(orRules).map(Rule::getStringOrRuleFunction).toSet());
+            } else {
+                return new AndRule(StreamEx.of(orRules[0].split(" ")).map(Integer::parseInt).toList());
+            }
+        }
+
+        boolean matches(String test, Map<Integer, Rule> rules);
+    }
+
+    @Value
+    @RequiredArgsConstructor
+    public static class OrRule implements Rule {
+        Set<Rule> subRules;
+        Map<String, Boolean> cache = new HashMap<>();
+
+        public OrRule(final Rule... subRules) {
+            this.subRules = StreamEx.of(subRules).toSet();
+        }
+
+        @Override
+        public boolean matches(final String test, final Map<Integer, Rule> rules) {
+            Boolean cached = cache.get(test);
+            if (cached == null) {
+                cached = evaluateMatch(test, rules);
+                cache.put(test, cached);
+            }
+
+            return cached;
+        }
+
+        private boolean evaluateMatch(final String s, final Map<Integer, Rule> rules) {
+            return StreamEx.of(this.subRules)
+                .anyMatch(rule -> rule.matches(s, rules));
+        }
+    }
+
+    @Value
+    @RequiredArgsConstructor
+    public static class AndRule implements Rule {
+        List<Integer> ruleIds;
+        Map<String, Boolean> cache = new HashMap<>();
+
+        public AndRule(final int... ruleIds) {
+            this.ruleIds = IntStreamEx.of(ruleIds).boxed().toList();
+        }
+
+        @Override
+        public boolean matches(final String test, final Map<Integer, Rule> rules) {
+            Boolean cached = cache.get(test);
+            if (cached == null) {
+                cached = evaluateMatch(test, rules);
+                cache.put(test, cached);
+            }
+
+            return cached;
+        }
+
+        private boolean evaluateMatch(final String s, final Map<Integer, Rule> rules) {
+            int startIndex = 0;
+            final List<Rule> subRules = ruleIds.stream()
+                .map(rules::get)
+                .collect(Collectors.toList());
+
+            for (final Rule rule : subRules) {
+                boolean ruleMatched = false;
+                for (int i = startIndex + 1; i < s.length() + 1; i++) {
+                    if (rule.matches(s.substring(startIndex, i), rules)) {
+                        startIndex = i;
+                        ruleMatched = true;
+                        break;
+                    }
+                }
+
+                if (!ruleMatched) {
+                    return false;
+                }
+            }
+
+            return startIndex == s.length();
+        }
+
+    }
+
+    @Value
+    public static class ConstRule implements Rule {
+        String value;
+
+        @Override
+        public boolean matches(final String s, final Map<Integer, Rule> rules) {
+            return value.equals(s);
+        }
+
     }
 }
--- src/test/java/AOC2020/Day19Test.java
+++ src/test/java/AOC2020/Day19Test.java
@@ -17,6 +17,33 @@ public class Day19Test {
     }
 
     @Test
+    public void a2() throws Exception {
+        day.setInputFilePath("/2020/day19-02.txt");
+
+        final Object result = day.getA();
+
+        assertEquals(3L, result);
+    }
+
+    @Test
+    public void b1() throws Exception {
+        day.setInputFilePath("/2020/day19-02.txt");
+
+        final Object result = day.getB();
+
+        assertEquals(12L, result);
+    }
+
+    @Test
+    public void b2() throws Exception {
+        day.setInputFilePath("/2020/day19-03.txt");
+
+        final Object result = day.getB();
+
+        assertEquals(12L, result);
+    }
+
+    @Test
     public void aFinal() throws Exception {
         final Object result = day.getA();
 
@@ -27,6 +54,6 @@ public class Day19Test {
     public void bFinal() throws Exception {
         final Object result = day.getB();
 
-        assertEquals(169899524778212L, result);
+        assertEquals(null, result);
     }
 }
