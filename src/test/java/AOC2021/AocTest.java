d300cf5adc2cf73b4ca65c7535d837e40c4f6d56
Ozin <Ozin@users.noreply.github.com>
2021-12-01 22:16:04 +0100

    Finish 2021/Day01

--- src/test/java/AOC2021/AocTest.java
+++ src/test/java/AOC2021/AocTest.java
@@ -1,15 +1,14 @@
 package AOC2021;
 
-import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
 
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -24,34 +23,27 @@ public interface AocTest {
         final String day = getClass().getSimpleName().substring(3, 5);
         final Object dayImplementation = Class.forName(getClass().getPackageName() + ".Day" + day).getDeclaredConstructor().newInstance();
 
-        return Stream.iterate(1, i -> i + 1)
+        return Stream.iterate(0, i -> i + 1)
                 .map(i -> String.format("/2021/day%s-%02d.yml", day, i))
-                .flatMap(testFile -> getDynamicTest(dayImplementation, testFile))
-                .takeWhile(Objects::nonNull);
-                /*
-                .map(i -> getClass().getResourceAsStream(String.format("/2021/day%s-%02d.yml", day, i)))
+                .map(filePath -> getClass().getResource(filePath))
                 .takeWhile(Objects::nonNull)
-                .map(is -> new YamlReader(new InputStreamReader(is)))
-                .map(this::readList)
-                .flatMap(l -> getDynamicTestStream(o, l));
-
-                 */
+                .flatMap(testFile -> getDynamicTest(dayImplementation, testFile));
     }
 
     @SneakyThrows
-    private Stream<DynamicTest> getDynamicTest(Object dayImplementation, String testFile) {
-        InputStream is = getClass().getResourceAsStream(testFile);
+    private Stream<DynamicTest> getDynamicTest(Object dayImplementation, URL is) {
         if (is == null) return Stream.empty();
 
-        List<String> testContent = new YamlReader(new InputStreamReader(is)).read(List.class);
+        List<String> testContent = new YamlReader(new InputStreamReader(is.openStream())).read(List.class);
 
+        final String testName = is.toString().substring(78);
         final DynamicTest getA = testContent.get(1) != null && Arrays.stream(dayImplementation.getClass().getDeclaredMethods()).map(Method::getName).anyMatch("a"::equals) ? dynamicTest(
-                testFile + " - getA",
+                testName + " - getA",
                 getExecutable(dayImplementation, "a", testContent.get(0), testContent.get(1))
         ) : null;
 
         final DynamicTest getB = testContent.get(2) != null && Arrays.stream(dayImplementation.getClass().getDeclaredMethods()).map(Method::getName).anyMatch("b"::equals) ? dynamicTest(
-                testFile + " - getB",
+                testName + " - getB",
                 getExecutable(dayImplementation, "b", testContent.get(0), testContent.get(2))
         ) : null;
 
