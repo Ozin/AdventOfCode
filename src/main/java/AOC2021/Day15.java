1aceaa0398450a059c0afc3ed22415808a5ca8d0
Ozin <Ozin@users.noreply.github.com>
2021-12-20 14:39:13 +0100

    Prepare 2021/Day15B optimized

--- src/main/java/AOC2021/Day15.java
+++ src/main/java/AOC2021/Day15.java
@@ -7,6 +7,7 @@ import utils.Point;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 
 public class Day15 {
 
@@ -18,20 +19,47 @@ public class Day15 {
             }
         }
 
-        return solveA(map, input[0].length() - 1, input.length - 1);
+        return solve(map::get, input[0].length(), input.length);
     }
 
-    private String solveA(final Map<Point, Integer> map, final int maxX, final int maxY) {
-        IntStreamEx.range(maxX + 1)
+    protected String b(final String[] input) throws Exception {
+        final Map<Point, Integer> map = new HashMap<>();
+        for (int y = 0; y < input.length; y++) {
+            for (int x = 0; x < input[0].length(); x++) {
+                map.put(new Point(x, y), Integer.parseInt(input[y].substring(x, x + 1)));
+            }
+        }
+
+        final int maxX = input[0].length();
+        final int maxY = input.length;
+
+        final Function<Point, Integer> calculatedMap = point -> {
+            if (point.getX() < 0 || point.getY() < 0) return null;
+
+            final int originalX = point.getX() % maxX;
+            final int originalY = point.getY() % maxY;
+            final int originalValue = map.get(new Point(originalX, originalY)) - 1;
+
+            final int newValue = (point.getX() / maxX + point.getY() / maxY
+                                  + originalValue) % 9;
+
+            return newValue + 1;
+        };
+
+        return solve(calculatedMap, maxX, maxY);
+    }
+
+    private String solve(final Function<Point, Integer> map, final int maxX, final int maxY) {
+        IntStreamEx.range(maxX)
                 .flatMapToObj(sum -> IntStreamEx.range(sum + 1).mapToObj(x -> new Point(x, sum - x)));
 
         final Map<Point, Integer> tentativeDistance = new HashMap<>();
         tentativeDistance.put(new Point(0, 0), 0);
 
-        for (int sum = 1; sum < 2 * maxX + 1; sum++) {
+        for (int sum = 1; sum < 2 * maxX; sum++) {
             for (int x = 0; x <= sum; x++) {
                 final Point current = new Point(x, sum - x);
-                final Integer currentValue = map.get(current);
+                final Integer currentValue = map.apply(current);
 
                 if (currentValue == null) continue;
 
@@ -39,7 +67,7 @@ public class Day15 {
             }
         }
 
-        return "" + tentativeDistance.get(new Point(maxX, maxY));
+        return "" + tentativeDistance.get(new Point(maxX - 1, maxY - 1));
     }
 
     private Integer minOf(final Integer x, final Integer y) {
@@ -47,10 +75,6 @@ public class Day15 {
                 .filter(Objects::nonNull)
                 .minByInt(i -> i)
                 .orElse(0);
-    }
-
-    protected String b(final String[] input) throws Exception {
-        return "";
     }
 
 }
