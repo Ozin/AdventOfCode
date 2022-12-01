2f21e8c339aa3b5a0e5289f65d2c58e78e481a31
Ozin <Ozin@users.noreply.github.com>
2022-12-01 10:07:51 +0100

    Start 2022

---
+++ src/main/java/AOC2021/Day19.java
@@ -0,0 +1,13 @@
+package AOC2021;
+
+
+public class Day19 {
+
+    protected String a(final String[] input) throws Exception {
+        return "" + null;
+    }
+
+    protected String b(final String[] input) throws Exception {
+        return "" + null;
+    }
+}
