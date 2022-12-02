a35a188ce8e3331faf3e0d087cf0d52d7dfb9850
Ozin <Ozin@users.noreply.github.com>
2022-12-02 10:32:40 +0100

    chore

--- src/test/java/AOC2021/AocTest.java
+++ src/test/java/AOC2021/AocTest.java
@@ -1,11 +1,11 @@
 package AOC2021;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -21,41 +21,53 @@ public interface AocTest {
     @TestFactory
     default Stream<DynamicTest> advent_of_code() throws Exception {
         final String day = getClass().getSimpleName().substring(3, 5);
-        final Object dayImplementation = Class.forName(getClass().getPackageName() + ".Day" + day).getDeclaredConstructor().newInstance();
+        final Object dayImplementation = Class.forName(getClass().getPackageName() + ".Day" + day)
+                                              .getDeclaredConstructor()
+                                              .newInstance();
 
         return Stream.iterate(0, i -> i + 1)
-                .map(i -> String.format("/2021/day%s-%02d.yml", day, i))
-                .map(filePath -> getClass().getResource(filePath))
-                .takeWhile(Objects::nonNull)
-                .flatMap(testFile -> getDynamicTest(dayImplementation, testFile));
+                     .map(i -> String.format("/2021/day%s-%02d.yml", day, i))
+                     .map(filePath -> getClass().getResource(filePath))
+                     .takeWhile(Objects::nonNull)
+                     .flatMap(testFile -> getDynamicTest(dayImplementation, testFile));
     }
 
-    @SneakyThrows
-    private Stream<DynamicTest> getDynamicTest(Object dayImplementation, URL is) {
+    private Stream<DynamicTest> getDynamicTest(final Object dayImplementation, final URL is) {
         if (is == null) return Stream.empty();
 
-        List<String> testContent = new YamlReader(new InputStreamReader(is.openStream())).read(List.class);
+        List<String> testContent = null;
+        try {
+            testContent = new YamlReader(new InputStreamReader(is.openStream())).read(List.class);
 
-        final String testName = is.toString().substring(78);
-        final DynamicTest getA = Arrays.stream(dayImplementation.getClass().getDeclaredMethods()).map(Method::getName).anyMatch("a"::equals) ? dynamicTest(
-                testName + " - getA",
-                getExecutable(dayImplementation, "a", testContent.get(0), testContent.get(1))
-        ) : null;
+            final String testName = is.toString().substring(78);
+            final DynamicTest getA = Arrays.stream(dayImplementation.getClass().getDeclaredMethods())
+                                           .map(Method::getName)
+                                           .anyMatch("a"::equals) ? dynamicTest(
+                    testName + " - getA",
+                    getExecutable(dayImplementation, "a", testContent.get(0), testContent.get(1))
+            ) : null;
 
-        final DynamicTest getB = Arrays.stream(dayImplementation.getClass().getDeclaredMethods()).map(Method::getName).anyMatch("b"::equals) ? dynamicTest(
-                testName + " - getB",
-                getExecutable(dayImplementation, "b", testContent.get(0), testContent.get(2))
-        ) : null;
+            final DynamicTest getB = Arrays.stream(dayImplementation.getClass().getDeclaredMethods())
+                                           .map(Method::getName)
+                                           .anyMatch("b"::equals) ? dynamicTest(
+                    testName + " - getB",
+                    getExecutable(dayImplementation, "b", testContent.get(0), testContent.get(2))
+            ) : null;
 
-        return Stream.of(
-                getA,
-                getB
-        ).filter(Objects::nonNull);
+            return Stream.of(
+                    getA,
+                    getB
+            ).filter(Objects::nonNull);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private Executable getExecutable(Object dayImplementation, String methodName, String testContent, String expected) {
+    private Executable getExecutable(final Object dayImplementation, final String methodName, final String testContent, final String expected) {
         return () -> {
-            final Object resultA = dayImplementation.getClass().getDeclaredMethod(methodName, String[].class).invoke(dayImplementation, (Object) testContent.split("\\n"));
+            final Object resultA = dayImplementation.getClass()
+                                                    .getDeclaredMethod(methodName, String[].class)
+                                                    .invoke(dayImplementation, (Object) testContent.split("\\n"));
             assertEquals(expected, resultA);
         };
     }
